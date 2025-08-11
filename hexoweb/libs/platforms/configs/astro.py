fad66b195af49782decca33d7afc117d01fcf38a
histcat <83163975+histcat@users.noreply.github.com>
2025-08-11 22:04:28 +0800

    Update astro.py

--- hexoweb/libs/platforms/configs/astro.py
+++ hexoweb/libs/platforms/configs/astro.py
@@ -6,7 +6,7 @@ config = {
         "depth": [-1],
         "type": [".md"],
         "save_path": "src/content/posts/${filename}.md",
-        "scaffold": "scaffolds/post.md"
+        "scaffold": "scaffold/post.md"
     },
     "drafts": {
         "path": [],
